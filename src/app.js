674cbcf58b906468dbfe0a97811ba16bc105f96b
Priyanshu Jha <pj4739479@gmail.com>
2025-06-01 14:21:10 +0530

    Exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -1,18 +1,37 @@
-const express = require('express');                           //importing the require from node_modules we are using express.js to create a server
+const express = require('express');                              //importing the require from node_modules we are using express.js to create a server
 
 const app = express();
 
-app.use("/hello",(req,res) => {                             //Add an API like "/test" 
 
-    res.send("Hello from the server!");                       //response as "Hello from the server!" using command express.use
+app.get("/",(req,res) => {
+
+    res.send("Welcome to the Dashboard!");
 
 });
 
-app.use("/test",(req,res) => {                           //Add an API like "/test"        
+app.get("/user", (req,res) => {                                //Add an API like "/test" 
+
+    res.send({firstname: "Priyanshu", lastname: "Jha"});      //response as "Hello from the server!" using command express.use
+
+});
+
+app.get("/hello", (req,res) => {                            //Add an API like "/test" 
+
+    res.send("Hello from the server!");                    //response as "Hello from the server!" using command express.use
+
+});
+
+app.get("/test", (req,res) => {                          //Add an API like "/test"        
 
     res.send("Testing the server!");                    //response as "Testing the server!" using command express.use
 
 });
+
+app.delete("/user", (req,res) => {
+    
+    res.send("User data deleted successfuly!");
+
+}); 
 
 app.listen(3000, () => {                              //callback as "Server listening to the port 3000...."
 
@@ -20,3 +39,4 @@ app.listen(3000, () => {                              //callback as "Server list
 
 });
 
+//Use app.get instead of app.use or we can just change the order of routes
