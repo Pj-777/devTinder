a266d6addb82c51baa965a8a0e3d4deb141612e5
Priyanshu Jha <pj4739479@gmail.com>
2025-06-05 12:27:31 +0530

    Exploring middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-const express = require('express');                              //importing the require from node_modules we are using express.js to create a server
+const express = require('express');                           //importing the require from node_modules we are using express.js to create a server
 
 const app = express();
-
 
 app.get("/",(req,res) => {
 
@@ -9,9 +8,18 @@ app.get("/",(req,res) => {
 
 });
 
-app.get("/user", (req,res) => {                                //Add an API like "/test" 
+app.get("/user1", (req,res) => {                                 //Add an API like "/test" 
 
-    res.send({firstname: "Priyanshu", lastname: "Jha"});      //response as "Hello from the server!" using command express.use
+    console.log(req.query);
+    res.send({firstname: "Priyanshu", lastname: "Jha"});       //response as "Hello from the server!" using command express.use
+    console.log("Hello World");
+
+});
+
+app.get("/user2/:userId/:name",(req,res) => {
+
+    console.log(req.params);
+    res.send({firstname: "Priyanshu", lastname: "Jha"});
 
 });
 
@@ -27,11 +35,45 @@ app.get("/test", (req,res) => {                          //Add an API like "/tes
 
 });
 
-app.delete("/user", (req,res) => {
+app.delete("/user1", (req,res) => {
     
     res.send("User data deleted successfuly!");
 
 }); 
+
+app.get(
+    "/user",
+    (req,res,next) => {
+       console.log("Handling route user 1!!");                    
+       next();
+       res.send("1st response!!");
+       
+    },
+
+    (req,res,next) => {
+       console.log("Handling route user 2!!");
+       //res.send("2nd response!!");
+       next();
+    },                                                                    
+    
+    (req,res,next) => {                                           //These functions in the middle are called 'middlewares'                      
+       console.log("Handling route user 3!!");                     
+       //res.send("3rd response!!");
+       next();
+    },
+    
+    (req,res,next) => {
+       console.log("Handling route user 4!!");
+       //res.send("4th response!!");
+       next();
+    },
+
+    (req,res,next) => {
+       console.log("Handling route user 5!!");             //This is route handler whcih actually handles the requests
+       //res.send("5th response!!");
+    },
+    
+);
 
 app.listen(3000, () => {                              //callback as "Server listening to the port 3000...."
 
@@ -39,4 +81,5 @@ app.listen(3000, () => {                              //callback as "Server list
 
 });
 
-//Use app.get instead of app.use or we can just change the order of routes
+//Use app.get instead of app.use for specific methods like GET,POST etc. or we can just change the order of routes
+//
